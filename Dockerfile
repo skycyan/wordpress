8d8131a3c7d062d017facc68b1a058dadcce9de8
adminops <xxxx@example.com>
2019-07-19 19:37:45 +0800

    add

---
+++ Dockerfile
@@ -0,0 +1,79 @@
+#FROM php:7.3-apache
+FROM layershop.dangdang.com/cnlab/php:7.3-apache
+
+# install the PHP extensions we need (https://make.wordpress.org/hosting/handbook/handbook/server-environment/#php-extensions)
+RUN set -ex; \
+	\
+        ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo 'Asia/Shanghai' >/etc/timezone; \
+        \
+	savedAptMark="$(apt-mark showmanual)"; \
+	\
+	apt-get update; \
+	apt-get install -y --no-install-recommends \
+		libjpeg-dev \
+		libmagickwand-dev \
+		libpng-dev \
+		libzip-dev \
+		net-tools \
+	; \
+	\
+	docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr; \
+	docker-php-ext-install -j "$(nproc)" \
+		bcmath \
+		exif \
+		gd \
+		mysqli \
+		opcache \
+		zip \
+	; \
+	pecl install imagick-3.4.4; \
+	docker-php-ext-enable imagick; \
+	\
+# reset apt-mark's "manual" list so that "purge --auto-remove" will remove all build dependencies
+	apt-mark auto '.*' > /dev/null; \
+	apt-mark manual $savedAptMark; \
+	ldd "$(php -r 'echo ini_get("extension_dir");')"/*.so \
+		| awk '/=>/ { print $3 }' \
+		| sort -u \
+		| xargs -r dpkg-query -S \
+		| cut -d: -f1 \
+		| sort -u \
+		| xargs -rt apt-mark manual; \
+	\
+	apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false; \
+	rm -rf /var/lib/apt/lists/*
+
+# set recommended PHP.ini settings
+# see https://secure.php.net/manual/en/opcache.installation.php
+RUN { \
+		echo 'opcache.memory_consumption=128'; \
+		echo 'opcache.interned_strings_buffer=8'; \
+		echo 'opcache.max_accelerated_files=4000'; \
+		echo 'opcache.revalidate_freq=2'; \
+		echo 'opcache.fast_shutdown=1'; \
+	} > /usr/local/etc/php/conf.d/opcache-recommended.ini
+# https://codex.wordpress.org/Editing_wp-config.php#Configure_Error_Logging
+RUN { \
+		echo 'error_reporting = 4339'; \
+		echo 'display_errors = Off'; \
+		echo 'display_startup_errors = Off'; \
+		echo 'log_errors = On'; \
+		echo 'error_log = /dev/stderr'; \
+		echo 'log_errors_max_len = 1024'; \
+		echo 'ignore_repeated_errors = On'; \
+		echo 'ignore_repeated_source = Off'; \
+		echo 'html_errors = Off'; \
+	} > /usr/local/etc/php/conf.d/error-logging.ini
+
+RUN a2enmod rewrite expires
+
+ADD . /var/www/html
+RUN set -ex; \
+    mkdir /usr/src/wordpress; \
+    cp -rfp /var/www/html/* /usr/src/wordpress; \
+    chown -R www-data:www-data /usr/src/wordpress
+
+COPY docker-entrypoint.sh /usr/local/bin/
+
+ENTRYPOINT ["docker-entrypoint.sh"]
+CMD ["apache2-foreground"]
---
+++ wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'my' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'my2019' );
+
+/** MySQL hostname */
+define( 'DB_HOST', '10.4.18.93:32000' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'S%SjzUQ4G(rxt|;> R!_``jJA48zGm5.JaO_ZW44y4_BObE7[OK#ik>;*aR+b+sP' );
+define( 'SECURE_AUTH_KEY',  '8_9LHv7=o)h-SKR2~^pJ2/#ayCR+jKJ+G9O#&}4A* ]-w%nz0{^zspb=^Faw!Dq)' );
+define( 'LOGGED_IN_KEY',    '&]CLoR<)I.`OB?<Mg#!)TjFEiZvCbx$Wdv2)]:8l,Q6mT[l*8gT?G!&|GNN0FrTP' );
+define( 'NONCE_KEY',        ' hSt/kDUZ@/6t?%0y8)n:2lc&huT?4*TNhX5BZ,/[?iH@^ohTnU2m1$R*}DB>F[>' );
+define( 'AUTH_SALT',        '~,U,d)l/b7k@gs]hB{}~28cKiaC=sV$K^Rv!di+p|d$-v}ylR~T`x/,[X{XzXfN;' );
+define( 'SECURE_AUTH_SALT', 'pC?Q_w9] 5464f e9o.6Hql.dl?I|_ubf>1o+}8L5qsm:kXPmZX7Dv16+jUTXVrx' );
+define( 'LOGGED_IN_SALT',   '0#[G~O?v9K9u99(t>LA9j>$#8lBwq~ }am=<B/?GC[yj{!3&}#q:`/?Ho^t6S~t[' );
+define( 'NONCE_SALT',       '7U%f1_Et3=H$~Pq,9-:d`t=*+g]ddvi3gIlhBEo35u`w2wf [;@<o1)emTmkr-Ej' );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wpq_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
